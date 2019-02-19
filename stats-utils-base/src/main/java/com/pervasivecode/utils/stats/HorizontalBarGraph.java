8d1d8f1d21a007c4a1a150ecc76f4d3581208ec4
Jamie Flournoy <jamie@pervasivecode.com>
2019-02-18 21:23:38 -0500

    Add Javadocs for HorizontalBarGraph.
    Make the constructor of HorizontalBarGraph and
    its builder have protected visibility.

--- stats-utils-base/src/main/java/com/pervasivecode/utils/stats/HorizontalBarGraph.java
+++ stats-utils-base/src/main/java/com/pervasivecode/utils/stats/HorizontalBarGraph.java
@@ -9,9 +9,28 @@ import com.google.auto.value.AutoValue;
 import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableList;
 
+/**
+ * A textual representation of a set of values, in the form of a bar graph growing rightward from
+ * an axis on left.
+ * <p>
+ * Each line of the graph consists of a label, a space, a bar, space-padding, and a formatted
+ * magnitude.
+ * <p>
+ * Example: A graph of width 20 with labels {"Ducks", "Goats", "Cows"}, magnitudes {3, 6, 8}, and
+ * formatted magnitudes {"three", "eight", "six"} would be formatted like this:
+ *
+ * <pre>
+ * Ducks ***      three
+ * Goats ******** eight
+ * Cows  ******   six
+ * </pre>
+ *
+ */
 @Immutable
 @AutoValue
 public abstract class HorizontalBarGraph {
+  protected HorizontalBarGraph() {}
+
   static class FormattingHints {
     int maxBarWidth = -1;
     int maxFormattedMagnitudeLength = -1;
@@ -33,6 +52,10 @@ public abstract class HorizontalBarGraph {
 
   protected abstract FormattingHints formattingHints();
 
+  /**
+   * Build the String representation of this bar graph and return it.
+   * @return The bar graph.
+   */
   public String format() {
     final int numRows = numRows();
     final int maxBarWidth = formattingHints().maxBarWidth;
@@ -66,11 +89,29 @@ public abstract class HorizontalBarGraph {
     return sb.toString();
   }
 
+  /**
+   * Obtain a builder that allows construction of a new instance.
+   * <p>
+   * The following properties are set to default values:
+   * <ul>
+   * <li>barPart = '*'
+   * </ul>
+   *
+   * @return A new, mostly-empty Builder instance.
+   */
   public static HorizontalBarGraph.Builder builder() {
     return new AutoValue_HorizontalBarGraph.Builder().setBarPart('*')
         .setFormattingHints(new FormattingHints());
   }
 
+  /**
+   * Obtain a builder that allows construction of a new instance, prepopulated with values from
+   * another instance.
+   *
+   * @param initialValues The instance whose values should be used to populate the new Builder
+   *        instance.
+   * @return a new Builder instance with the same values as the {@code initialValues} parameter.
+   */
   public static HorizontalBarGraph.Builder builder(HorizontalBarGraph initialValues) {
     return builder().setBarPart(initialValues.barPart()) //
         .setFormattedMagnitudes(initialValues.formattedMagnitudes()) //
@@ -81,24 +122,83 @@ public abstract class HorizontalBarGraph {
         .setWidth(initialValues.width());
   }
 
+  /**
+   * An object that can be used to create a {@link HorizontalBarGraph}.
+   */
   @AutoValue.Builder
   public static abstract class Builder {
+    protected Builder() {}
+
+    /**
+     * Set the character width of the graph.
+     *
+     * @param width The desired width of the graph in characters.
+     * @return A builder that can be used to finish creating a {@link HorizontalBarGraph} instance.
+     */
     public abstract Builder setWidth(int width);
 
+    /**
+     * Set the number of data rows in the graph. This specifies the required number of elements in
+     * the labels, magnitudes, and formattedMagnitudes lists that are provided to this builder.
+     *
+     * @param numRows The number of data rows.
+     * @return A builder that can be used to finish creating a {@link HorizontalBarGraph} instance.
+     */
     public abstract Builder setNumRows(int numRows);
 
+    /**
+     * Set the character used to render the bar (which is a variable-width representation of each
+     * row's magnitude value).
+     *
+     * @param barPart The character to use to render the bar.
+     * @return A builder that can be used to finish creating a {@link HorizontalBarGraph} instance.
+     */
     public abstract Builder setBarPart(char barPart);
 
+    /**
+     * Set the labels used to identify which value each row represents.
+     *
+     * @param labels The labels that identify what value each row represents.
+     * @return A builder that can be used to finish creating a {@link HorizontalBarGraph} instance.
+     */
     public abstract Builder setLabels(List<String> labels);
 
+    /**
+     * Set the magnitudes of the values shown as bars of varying lengths. The largest magnitude
+     * from this list will be shown as a full-width bar, and all other magnitudes are shown as bars
+     * with shorter bars, in linear proportion to that largest value.
+     *
+     * @param magnitudes The values that control the width of the bars.
+     * @return A builder that can be used to finish creating a {@link HorizontalBarGraph} instance.
+     */
     public abstract Builder setMagnitudes(List<Long> magnitudes);
 
+    /**
+     * Set the formatted version of the magnitude, which is shown to the right of the bar.
+     *
+     * @param formattedMagnitudes The formatted representation of the magnitude value.
+     * @return A builder that can be used to finish creating a {@link HorizontalBarGraph} instance.
+     */
     public abstract Builder setFormattedMagnitudes(List<String> formattedMagnitudes);
 
+    /**
+     * Derived values that are used in formatting, which are also used by validation code. This is
+     * a field in order to avoid code duplication, so that the values can be computed once and then
+     * used by validation and again by formatting logic.
+     *
+     * @param hints The derived values.
+     * @return A builder that can be used to finish creating a {@link HorizontalBarGraph} instance.
+     */
     protected abstract Builder setFormattingHints(FormattingHints hints);
 
     protected abstract HorizontalBarGraph buildInternal();
 
+    /**
+     * Validate the contents of this Builder and create a {@link HorizontalBarGraph} instance.
+     *
+     * @return An immutable HorizontalBarGraph instance that can be used to produce the formatted
+     *         representation of the values in this Builder.
+     */
     public HorizontalBarGraph build() {
       final HorizontalBarGraph unvalidated = buildInternal();
 
